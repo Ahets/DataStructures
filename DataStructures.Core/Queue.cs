06b342b5afdb645b1c75f6764835684c64a78563
Ahets <yura.tashko98@gmail.com>
2017-05-13 23:19:18 +0300

    Initial commit

---
+++ DataStructures.Core/Queue.cs
@@ -0,0 +1,60 @@
+﻿using DataStructures.Core.Contracts;
+using System;
+
+namespace DataStructures.Core
+{
+    public class Queue<T> : IQueue<T>
+    {
+
+        private class QueueItem
+        {
+            public T Value { get; }
+            public QueueItem Next { get; set; }
+
+            public QueueItem(T value)
+            {
+                Value = value;
+            }
+        }
+
+        private QueueItem tail;
+        private QueueItem head;
+
+        public void Push(T item)
+        {
+            var newItem = new QueueItem(item);
+            if (tail != null)
+                tail.Next = newItem;
+            tail = newItem;
+            if (head == null)
+                head = newItem;
+        }
+
+        public T Pop()
+        {
+            if (Empty)
+                throw new InvalidOperationException("Queue is empty");
+            var oldItem = head;
+            head = oldItem.Next;
+            return oldItem.Value;
+        }
+
+        public T Top
+        {
+            get
+            {
+                if (Empty)
+                    throw new InvalidOperationException("Queue is empty");
+                return head.Value;
+            }
+        }
+
+        public bool Empty
+        {
+            get
+            {
+                return head == null;
+            }
+        }
+    }
+}
---
+++ DataStructures.Tests/ListTests.cs
@@ -0,0 +1,28 @@
+﻿using DataStructures.Core;
+using NUnit.Framework;
+
+namespace DataStructures.Tests
+{
+    [TestFixture]
+    public class ListTests
+    {
+        [Test]
+        public void AddElementsToBack()
+        {
+            var list = new List<int>();
+
+            Assert.IsTrue(list.Empty);
+
+            list.PushTail(1);
+
+            Assert.IsFalse(list.Empty);
+            Assert.AreEqual(1, list.Head);
+            Assert.AreEqual(1, list.Tail);
+
+            list.PushTail(2);
+
+            Assert.AreEqual(1, list.Head);
+            Assert.AreEqual(2, list.Tail);
+        }
+    }
+}
