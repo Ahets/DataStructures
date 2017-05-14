78845d164c24be3ad3d5fb301ee3fee18a56f7d8
Ahets <yura.tashko98@gmail.com>
2017-05-14 17:10:32 +0300

    List Implementation

--- DataStructures.Core/List.cs
+++ DataStructures.Core/List.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace DataStructures.Core
@@ -7,21 +8,71 @@ namespace DataStructures.Core
     {
         private class ListItem
         {
-            private T Value { get; }
-            private ListItem Previous { get; }
-            private ListItem Next { get; }
+            public T Value { get; }
+            public ListItem Previous { get; set; }
+            public ListItem Next { get; set; }
 
-            private ListItem(ListItem previous, ListItem next)
+            public ListItem(ListItem previous, ListItem next, T value)
             {
                 Previous = previous;
                 Next = next;
+                Value = value;
             }
         }
 
-        private ListItem
+        private class ListEnumerator : IEnumerator<T>
+        {
+            private ListItem currentItem;
+            private ListItem head;
+
+            public ListEnumerator(ListItem item)
+            {
+                head = item;
+            }
+
+            public void Dispose()
+            {
+                head = null;
+                currentItem = null;
+            }
+
+            public bool MoveNext()
+            {
+                if (currentItem == null)
+                {
+                    currentItem = head;
+                    return head != null;
+                }
+
+                if (currentItem.Next == null)
+                    return false;
+                currentItem = currentItem.Next;
+                return true;
+            }
+
+            public void Reset()
+            {
+
+                currentItem = null;
+            }
+
+            public T Current
+            {
+                get { return currentItem.Value; }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+        }
+
+        private ListItem head;
+        private ListItem tail;
+
         public IEnumerator<T> GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            return new ListEnumerator(head);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -31,26 +82,76 @@ namespace DataStructures.Core
 
         public void PushHead(T item)
         {
-            throw new System.NotImplementedException();
+            var newHeadItem = new ListItem(null, head, item);
+            if (head != null)   
+                head.Previous = newHeadItem;
+            head = newHeadItem;
+            if (tail == null)
+                tail = newHeadItem;
         }
 
         public void PushTail(T item)
         {
-            throw new System.NotImplementedException();
+            var newTailItem = new ListItem(tail, null, item);
+            if (tail != null)
+                tail.Next = newTailItem;
+            tail = newTailItem;
+            if (head == null)
+                head = newTailItem;
         }
 
         public T PopHead()
         {
-            throw new System.NotImplementedException();
+            if (Empty)
+                throw new InvalidOperationException("List is empty");
+            var oldHeadItem = head;
+            head = oldHeadItem.Next;
+            if (head != null)
+                head.Previous = null;
+            else
+                tail = null;
+            return oldHeadItem.Value;
         }
 
         public T PopTail()
         {
-            throw new System.NotImplementedException();
+            if (Empty)
+                throw new InvalidOperationException("List is empty");
+            var oldTailItem = tail;
+            tail = oldTailItem.Previous;
+            if (tail != null)
+                tail.Next = null;
+            else
+                head = null;
+            return oldTailItem.Value;
         }
 
-        public T Head { get; }
-        public T Tail { get; }
-        public bool Empty { get; }
+        public T Head
+        {
+            get
+            {
+                if (Empty)
+                    throw new InvalidOperationException("List is empty");
+                return head.Value;
+            }
+        }
+
+        public T Tail
+        {
+            get
+            {
+                if (Empty)
+                    throw new InvalidOperationException("List is empty");
+                return tail.Value;
+            }
+        }
+
+        public bool Empty
+        {
+            get
+            {
+                return tail == null;
+            }
+        }
     }
 }
